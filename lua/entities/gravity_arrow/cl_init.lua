1577c83117d2f1dcc4b3075fe90cf044b5e9c5db
viral32111 <19510403+viral32111@users.noreply.github.com>
2021-01-01 12:13:22 +0000

    Update copyright notice to 2021

--- lua/entities/gravity_arrow/cl_init.lua
+++ lua/entities/gravity_arrow/cl_init.lua
@@ -1,6 +1,6 @@
 --[[------------------------------------------------------------------------
 Gravity Arrow - A small blue arrow that controls the force of world gravity.
-Copyright (C) 2020 viral32111 (https://viral32111.com)
+Copyright (C) 2020 - 2021 viral32111 (https://viral32111.com)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as
